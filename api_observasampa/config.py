7519bb2bf160db1bc48c24d9563c83d060b49c76
rossibruno <96496152+rossibruno@users.noreply.github.com>
2024-09-17 19:13:33 -0300

    Update config.py

--- api_observasampa/config.py
+++ api_observasampa/config.py
@@ -1,32 +1,29 @@
 from core.utils.utils import build_conn_str
-from dotenv import load_dotenv
 import os
 
-# Carrega as variáveis de ambiente do arquivo .env
-load_dotenv()
+# Verifica se está em ambiente de desenvolvimento
+ENV = os.getenv('ENV', 'DEVELOPMENT')
 
-# Função para verificar se todas as variáveis de ambiente estão definidas
+if ENV == 'DEVELOPMENT':
+    from dotenv import load_dotenv
+    load_dotenv()
+
 def check_env_vars():
     required_vars = ['PGHOST', 'PGPORT', 'PGDATABASE', 'PGUSER', 'PGPASSWORD', 'REQUEST_TIMEOUT_ERROR']
     for var in required_vars:
         if not os.getenv(var):
-            raise EnvironmentError(f"Variável de ambiente '{var}' não está definida. Verifique seu arquivo .env.")
+            raise EnvironmentError(f"Variável de ambiente '{var}' não está definida. Verifique suas variáveis de ambiente.")
 
-# Verifica se todas as variáveis de ambiente necessárias estão presentes
 check_env_vars()
 
-# Dados do banco de dados
 db_data = {
-    'host': os.environ['PGHOST'],  # Adaptar para usar PGHOST
+    'host': os.environ['PGHOST'],
     'port': os.environ['PGPORT'],
     'database': os.environ['PGDATABASE'],
     'user': os.environ['PGUSER'],
     'password': os.environ['PGPASSWORD']
 }
 
-
-# Construa a URL do SQLAlchemy com base nos dados do banco
 SQLALCHEMY_DATABASE_URL = build_conn_str(**db_data)
 
-# Valor do tempo limite da requisição
-REQUEST_TIMEOUT_ERROR = int(os.getenv('REQUEST_TIMEOUT_ERROR', 500))  # Define 500 como valor padrão se não for encontrado
+REQUEST_TIMEOUT_ERROR = int(os.getenv('REQUEST_TIMEOUT_ERROR', 500))
